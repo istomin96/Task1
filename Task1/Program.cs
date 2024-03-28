f69e2f5202a09785ae07ccba5f7a37406b8e1288
Roman Istomin <roma.istomin.96@bk.ru>
2024-03-28 09:59:31 +0500

    С рабочего компьютера

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,20 +1,48 @@
 ﻿using System.Drawing;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using static Task1.Program;
 
 namespace Task1
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"D:\Папка с файлами\");
+            //Задание 1, 2, 3
 
-            Delete(dirInfo);
+            string path = @"D:\Папка с файлами\";
 
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            Delete(dirInfo, path);
+
+            //Задание 4
+
+            string path2 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Students.dat";
+
+            List<Student> studentsToWrite = new List<Student>
+            {
+                new Student { Name = "Софья", Group = "Группа 1", DateOfBirth = new DateTime(1996, 10, 12), AverageScore = 3.9M },
+                new Student { Name = "Владимир", Group = "Группа 1", DateOfBirth = new DateTime(1988, 06, 14), AverageScore = 4.0M},
+                new Student { Name = "Сергей", Group = "Группа 2", DateOfBirth = new DateTime(1998, 02, 24), AverageScore = 4.2M},
+                new Student { Name = "Дмитрий", Group = "Группа 2", DateOfBirth = new DateTime(2000, 8, 26), AverageScore = 4.7M}
+            };
+
+            WriteStudents(studentsToWrite, path2);
+
+            List<Student> studentsToRead = ReadStudents(path2);
+
+            foreach (Student studentProp in studentsToRead)
+            {
+                Console.WriteLine(studentProp.Name + " " + studentProp.Group + " " + studentProp.DateOfBirth + " " + studentProp.AverageScore);
+            }
         }
 
-        //Метод удаления папки + добавлен метод по размеру папки
-        static void Delete(DirectoryInfo dir)
+        //Задание 1. Метод удаления папки + Задание 3. Добавлен метод по размеру папки
+        static void Delete(DirectoryInfo dir, string path)
         {
             if (dir.Exists)
             {
@@ -39,7 +67,7 @@ namespace Task1
             }
         }
 
-        //Метод размера папки
+        //Задание 2. Метод размера папки
         static long DirSize(DirectoryInfo dir)
         {
             long size = default;
@@ -64,14 +92,65 @@ namespace Task1
             return size;
         }
 
-
-        static void Students()
+        //Задание 4. Программа бинарного формата текста
+        internal class Student
         {
-            string Name;
-            string Group;
-            DateTime DateOfBirth;
-            decimal Dec;
+            public string Name { get; set; }
+            public string Group { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public decimal AverageScore { get; set; }
 
+        }
+
+        static void WriteStudents(List<Student> students, string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                using (File.Create(fileName));
+            }
+           
+            using FileStream fs = new FileStream(fileName, FileMode.Create);
+            using BinaryWriter bw = new BinaryWriter(fs);
+
+            foreach (Student student in students)
+            {
+                bw.Write(student.Name);
+                bw.Write(student.Group);
+                bw.Write(student.DateOfBirth.ToBinary());
+                bw.Write(student.AverageScore);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+
+        static List<Student> ReadStudents(string fileName)
+        {
+            List<Student> result = new();
+            using FileStream fs = new FileStream(fileName, FileMode.Open);
+            using StreamReader sr = new StreamReader(fs);
+
+            Console.WriteLine(sr.ReadToEnd());
+
+            fs.Position = 0;
+
+            BinaryReader br = new BinaryReader(fs);
+
+            while (fs.Position < fs.Length)
+            {
+                Student student = new Student();
+                student.Name = br.ReadString();
+                student.Group = br.ReadString();
+                long dt = br.ReadInt64();
+                student.DateOfBirth = DateTime.FromBinary(dt);
+                student.AverageScore = br.ReadDecimal();
+
+                result.Add(student);
+            }
+
+            fs.Close();
+
+            return result;
         }
     }
 }
